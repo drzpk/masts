a23f7f050c1591f4672acba8580cc7fba946edae
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2026-01-24 13:12:52 +0100

    Add 2nd mast and configure pins

--- src/config.h
+++ src/config.h
@@ -4,24 +4,38 @@
 #include "../component/status_leds.h"
 #include "../readout/readout.h"
 
-#define DEBUG_ANALOG_READOUT true
+#define DEBUG_ANALOG_READOUT false
 
 MastConfig mast1Config = {
   .mastId = 1,
-  .pinValveUp = A7,
-  .pinValveDown = D9,
-  .pinLedUp = D12,
-  .pinLedDown = D11,
-  .pinButtonUp = D2,
-  .pinButtonDown = D10,
+  .pinValveUp = A0,
+  .pinValveDown = A1,
+  .pinLedUp = D5,
+  .pinLedDown = D7,
+  .pinButtonUp = D4,
+  .pinButtonDown = D6,
+  .raiseTimeMs = 3000,
+  .lowerTimeMs = 2000
+};
+
+MastConfig mast2Config = {
+  .mastId = 2,
+  .pinValveUp = A2,
+  .pinValveDown = A3,
+  .pinLedUp = D10,
+  .pinLedDown = D12,
+  .pinButtonUp = D9,
+  .pinButtonDown = D11,
   .raiseTimeMs = 3000,
   .lowerTimeMs = 2000
 };
 
 Mast mast1(mast1Config);
+Mast mast2(mast2Config);
 
 const MastState* mastStates[] = {
-  &mast1.getState()
+  &mast1.getState(),
+  &mast2.getState()
 };
 
 StatusLedsConfig statusLedsConfig = {
@@ -32,6 +46,7 @@ StatusLedsConfig statusLedsConfig = {
 
 Component* components[] = {
   &mast1,
+  &mast2,
   new StatusLeds(statusLedsConfig),
 #if DEBUG_ANALOG_READOUT
   new Readout(A0),
